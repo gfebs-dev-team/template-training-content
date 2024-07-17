ce2ec31857875f01996e3ad58b31a5f86a15b593
PaulOrecchia <paulaorecchia@gmail.com>
2024-07-17 09:36:58 -0400

    update css to Tailwind

--- src/components/SlideSection.vue
+++ src/components/SlideSection.vue
@@ -1,6 +1,6 @@
 <script setup>
 import SlideBase from './SlideBase.vue'
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
 const hasImg = ref(false)
 
 const props = defineProps(['title', 'img', 'subtitle', 'section', 'sectionNum', 'type'])
@@ -13,16 +13,9 @@ onMounted(() => {
 
 <template>
   <SlideBase class="content" v-bind="props" :columns="1">
-      <div class="section-title" title>
+      <div class="m-auto mt-44" title>
         <h2 class="text-5xl w-full text-saffron font-bold">{{ sectionNum }}</h2>
         <h3 class="text-4xl w-full font-bold">{{ subtitle }}</h3>
       </div>
   </SlideBase>
 </template>
-
-<style scoped lang="scss">
-.section-title{
-  margin: auto;
-  margin-top: 11rem;
-}
-</style>
