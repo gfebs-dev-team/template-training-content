355d7cc98eba53f2d919972bd1ed3e8d3d7a5f74
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-09-13 09:51:51 -0400

    minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,40 @@
 <script setup>
-import ContentContainer from './components/ContentContainer.vue'
+import ContentHeader from '@/components/ContentHeader.vue'
+import AppGlossary from '@/components/AppGlossary.vue'
+import AppNav from '@/components/AppNav.vue'
+import SideBar from '@/components/SideBar.vue'
+import SlideContainer from '@/components/SlideContainer.vue'
+import glossary from './assets/glossary.json'
+import { useSlidesStore } from '@/stores/slides'
+import { storeToRefs } from 'pinia'
 
 const courseData = {
   courseCode: ' ',
-  courseTitle: 'Essentials',
-  topic: 'Introduction'
+  courseTitle: 'Training Content Template',
+  topic: 'Demo'
 }
+
+const slides = useSlidesStore()
+const { glossaryState, sidebarState } = storeToRefs(slides)
+const { toggleSidebar } = slides
 </script>
 
 <template>
-  <ContentContainer :courseData="courseData" />
+  <main
+    class="flex h-dvh flex-col justify-between xl:items-center xl:bg-spacecadet xl:px-32 xl:py-10"
+  >
+    <AppGlossary :glossary="glossary" v-if="glossaryState"></AppGlossary>
+
+    <ContentHeader @toggleSidebar="toggleSidebar()" :courseData="courseData"></ContentHeader>
+    <section
+      class="relative flex size-full h-full flex-col justify-between overflow-hidden xl:rounded-b-lg bg-oxfordblue xl:max-w-[1200px]"
+    >
+      <SideBar :sidebarState="sidebarState" />
+
+      <SlideContainer />
+    </section>
+
+    <AppNav :courseData="courseData" />
+    <!-- Bottom Nav -->
+  </main>
 </template>
--- src/components/ContentHeader.vue
+++ src/components/ContentHeader.vue
@@ -21,26 +21,27 @@ function openLink(link) {
 function disable() {
   var dis = true
   if (testing) {
-    dis = !(current.value < (total - 1))
-  }
-  else {
+    dis = !(current.value < total - 1)
+  } else {
     dis = !slides.next
   }
 
   return dis
 }
 
-
 defineProps(['courseData'])
 </script>
 
 <template>
-  <div class="z-10 box-content flex w-full flex-col items-center justify-end bg-spacecadet xl:gap-4 xl:max-w-[1200px]">
-    <div class="grid h-min sm:h-20 w-full grid-cols-[auto_auto_1fr] items-center gap-4 p-4 xl:h-auto">
+  <div
+    class="z-10 box-content flex w-full flex-col items-center justify-end bg-spacecadet xl:gap-4 xl:max-w-[1200px]"
+  >
+    <div
+      class="grid h-min sm:h-20 w-full grid-cols-[auto_auto_1fr] items-center gap-4 p-4 xl:h-auto"
+    >
       <img class="h-8 md:h-12 xl:h-16" src="/gfebs_white.png" />
       <div class="flex flex-col gap-1">
-        <h1
-          class="max-w-56 text-balance text-sm font-bold uppercase text-aliceblue md:text-base xl:max-w-64 xl:text-xl">
+        <h1 class="text-balance text-sm font-bold uppercase text-aliceblue md:text-base xl:text-xl">
           GFEBS {{ courseData.courseCode }}
           {{ courseData.courseTitle }}
         </h1>
@@ -50,42 +51,61 @@ defineProps(['courseData'])
       </div>
       <div class="flex flex-col gap-4 justify-self-end xl:w-2/3">
         <AppProgress class="hidden xl:block" />
-        <div class="grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 w-full justify-end gap-1 md:gap-4">
-          <AppPopover class-name="order-2 sm:order-none"
-            buttonStyle="relative max-w-36 h-full justify-self-end border-masblue border-3 text-coolgrey p-1 px-2 text-xs text-aliceblue lg:text-sm flex xl:w-full xl:max-w-none ">
+        <div
+          class="grid grid-rows-2 sm:grid-rows-1 sm:grid-cols-2 w-full justify-end gap-1 md:gap-4"
+        >
+          <AppPopover
+            class-name="order-2 sm:order-none"
+            buttonStyle="relative max-w-36 h-full justify-self-end border-masblue border-3 text-coolgrey p-1 px-2 text-xs text-aliceblue lg:text-sm flex xl:w-full xl:max-w-none "
+          >
             <template #button-name>
               <RiDatabaseLine class="size-5 fill-coolgrey" /> Resources
             </template>
             <template #menu>
-              <a v-if="glossaryEnable"
+              <a
+                v-if="glossaryEnable"
                 class="font-bold p-2 rounded hover:bg-masblue transition-colors text-xs text-aliceblue md:text-sm xl:w-full xl:max-w-none"
-                @click="glossaryState = !glossaryState">Glossary</a>
-              <a class="font-bold p-2 rounded hover:bg-masblue transition-colors text-xs text-aliceblue md:text-sm xl:w-full xl:max-w-none"
-                @click="openLink('https://gfebs.army.mil/gm')">GFEBS Production PSW</a>
+                @click="glossaryState = !glossaryState"
+                >Glossary</a
+              >
+              <a
+                class="font-bold p-2 rounded hover:bg-masblue transition-colors text-xs text-aliceblue md:text-sm xl:w-full xl:max-w-none"
+                @click="openLink('https://gfebs.army.mil/gm')"
+                >GFEBS Production PSW</a
+              >
             </template>
           </AppPopover>
           <AppButton
             class="w-full justify-self-end border-masblue border-3 text-saffron bg-transparent p-1 px-2 text-xs md:text-sm xl:w-full xl:max-w-none"
-            @click="windowClose()">
+            @click="windowClose()"
+          >
             <RiCloseLine class="size-6 fill-saffron" /> EXIT
           </AppButton>
         </div>
       </div>
     </div>
     <div
-      class="grid h-16 w-full grid-cols-[1.5rem_1fr_auto] items-center gap-4 border-t border-masblue p-4 shadow-md transition md:h-12 md:py-1 xl:h-16 xl:rounded-t-lg xl:border-t-0 xl:bg-masblue">
-      <RiMenuLine @click="$emit('toggleSidebar')" class="size-6 fill-coolgrey p-1 hover:cursor-pointer xl:size-8">
+      class="grid h-16 w-full grid-cols-[1.5rem_1fr_auto] items-center gap-4 border-t border-masblue p-4 shadow-md transition md:h-12 md:py-1 xl:h-16 xl:rounded-t-lg xl:border-t-0 xl:bg-masblue"
+    >
+      <RiMenuLine
+        @click="$emit('toggleSidebar')"
+        class="size-6 fill-coolgrey p-1 hover:cursor-pointer xl:size-8"
+      >
       </RiMenuLine>
 
       <div class="hidden h-12 items-center justify-end gap-4 text-xs text-aliceblue xl:flex">
         <AppButton
           class="h-fit max-w-fit border-2 border-aliceblue p-2 px-4 disabled:border-coolgrey disabled:text-coolgrey"
-          @click="goPrev()" :disabled="!slides.prev">
+          @click="goPrev()"
+          :disabled="!slides.prev"
+        >
           Back
         </AppButton>
         <AppButton
           class="h-fit max-w-fit border-2 border-aliceblue p-2 px-4 disabled:border-coolgrey disabled:text-coolgrey"
-          @click="goNext()" :disabled="disable()">
+          @click="goNext()"
+          :disabled="disable()"
+        >
           Next
         </AppButton>
       </div>
