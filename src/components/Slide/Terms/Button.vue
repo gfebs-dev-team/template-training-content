be4232d313fa6551c93932a3d31f471b428f7f9c
Rhylen Nguyen <rhylennguyen@gmail.com>
2024-10-08 11:57:07 -0400

    disable button hover on active

--- src/components/Slide/Terms/Button.vue
+++ src/components/Slide/Terms/Button.vue
@@ -14,7 +14,7 @@ const viewed = ref(false)
     :class="
       twMerge(
         'relative outline-none p-4 min-w-0 min-h-0 rounded-md text-center text-xs sm:text-base cursor-pointer border-2 border-transparent hover:bg-transparent hover:border-saffron hover:text-aliceblue bg-spacecadet text-aliceblue font-bold transition-all',
-        currIndex === index ? 'bg-saffron text-spacecadet' : ''
+        currIndex === index ? 'bg-saffron text-spacecadet pointer-events-none' : ''
       )
     "
   >
